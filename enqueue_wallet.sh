244999f2a5857d63cef9c6cb7e675a14e04e6215
ninosanta <38584348+ninosanta@users.noreply.github.com>
2018-05-21 22:25:33 +0200

    fixed argument passed to an echo

--- enqueue_wallet.sh
+++ enqueue_wallet.sh
@@ -4,7 +4,7 @@ echo -n "Amount: "; read amount
 echo -n "Description: "; read des
 echo -e "$amount\t\t$(date +%x)\t$des"
 echo -e "$amount\t\t$(date +%x)\t$des" >> "wallet.txt"
-echo -"\nLast 5 movements:"
+echo -e "\nLast 5 movements:"
 tail -5 "wallet.txt"
 sleep 5
 echo -e "\n\n"
